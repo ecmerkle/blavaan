3ca47e7df05126ccf1ccfb60c8fcff3954925a9e
ecmerkle <merklee@missouri.edu>
2018-05-22 11:46:18 -0500

    close the parenthesis

--- R/blavaan.R
+++ R/blavaan.R
@@ -45,7 +45,7 @@ blavaan <- function(...,  # default lavaan arguments
     }
 
     # ordinal functionality not available
-    if("ordered" %in% dotNames) stop("blavaan ERROR: models with ordered variables are not yet available."
+    if("ordered" %in% dotNames) stop("blavaan ERROR: models with ordered variables are not yet available.")
   
     # ensure rstan/runjags are here. if target is not installed but
     # the other is, then use the other instead.
