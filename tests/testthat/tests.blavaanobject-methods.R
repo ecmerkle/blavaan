5f49369bdbbd2665aebe0d32b935b2ba079c541f
ecmerkle <merklee@missouri.edu>
2018-09-05 15:18:18 -0500

    remove unnecessary library()

--- tests/testthat/tests.blavaanobject-methods.R
+++ tests/testthat/tests.blavaanobject-methods.R
@@ -2,8 +2,6 @@ test_that("blavaan object methods work", {
   fitj <- blavaan:::fitjags
   fits <- blavaan:::fitstan
 
-  library("blavaan")
-  
   expect_equal(fitjags@ParTable$free, fitstan@ParTable$free)
 
 })
