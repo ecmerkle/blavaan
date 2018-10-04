5ebde476b15d222c34857c268650a4dcc72c03bf
ecmerkle <merklee@missouri.edu>
2018-10-03 22:06:38 -0500

    add to tests

--- tests/testthat/tests.blavaanobject-methods.R
+++ tests/testthat/tests.blavaanobject-methods.R
@@ -6,7 +6,26 @@ test_that("blavaan object methods work", {
     #fitj <- blavaan:::fitjags
     #fits <- blavaan:::fitstan
 
+    # classes
+    expect_equal(class(fitjags@external), "list")
+    expect_equal(class(fitstan@external), "list")
+
+    ## parameter summaries
+    expect_equal(dim(parTable(fitjags)), c(10,20))
+    expect_equal(dim(parTable(fitstan)), c(10,25))
+
+    expect_equal(sum(fitjags@ParTable$free > 0, na.rm = TRUE),
+                 length(blavInspect(fitjags, 'psrf')))
+    expect_equal(sum(fitstan@ParTable$free > 0, na.rm = TRUE),
+                 length(blavInspect(fitstan, 'psrf')))
     expect_equal(fitjags@ParTable$free, fitstan@ParTable$free)
+    expect_equal(nrow(parTable(fitjags)), nrow(parTable(fitstan)))
+    
+    expect_error(blavInspect(fitjags, 'blah'))
+
+    ## fitMeasures
+    expect_equal(length(fitMeasures(fitjags)),
+                 length(fitMeasures(fitstan)))
   }
     
 })
