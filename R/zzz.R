a5d0130366f9be2d67c50ddce76a39f37d000a04
ecmerkle <merklee@missouri.edu>
2019-07-14 10:29:01 -0500

    informative startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
     version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
                       fields="Version")
     packageStartupMessage("This is ",paste(pkgname, version))
-    packageStartupMessage("Note: The default target is now 'stan', which may break code using old blavaan versions.\n (To use JAGS, you now need to explicitly state target = 'jags'.)")
+    packageStartupMessage("Major changes from blavaan 0.3-4:\n  - The default target is now 'stan' instead of 'jags'.\n  - To use JAGS, you now need to explicitly set target = 'jags'.\n  - To use the old Stan approach, set target = 'stanclassic'.\n  - Priors on variance parameters now default to gamma(1,.5) on the SD.")
 }
 
 .onLoad <- function(libname, pkgname) {
