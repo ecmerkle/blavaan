81ea76c0e40001d88a1b91953d18a4656fe5c2bb
ecmerkle <merklee@missouri.edu>
2019-08-06 15:22:04 -0500

    fix for stan plot bug

--- R/blav_object_methods.R
+++ R/blav_object_methods.R
@@ -348,15 +348,15 @@ plot.blavaan <- function(x, pars=NULL, plot.type="trace", ...){
                   ddd))
     } else {
         parnums <- x@ParTable$stanpnum[match(pars, x@ParTable$free)]
-        parlabs <- with(x@ParTable, paste0(lhs,op,rhs)[match(pars, free)])
-        oldnames <- names(x@external$mcmcout)[parnums]
-        names(x@external$mcmcout)[parnums] <- parlabs
+        #parlabs <- with(x@ParTable, paste0(lhs,op,rhs)[match(pars, free)])
+        parlabs <- names(x@external$mcmcout)[parnums]
+        #names(x@external$mcmcout)[parnums] <- parlabs
         plargs <- list(x = x@external$mcmcout,
                        plotfun = plot.type)
         plargs <- c(plargs, list(pars = parlabs), list(...))
 
         do.call(rstan::plot, plargs)
-        names(x@external$mcmcout)[parnums] <- oldnames
+        #names(x@external$mcmcout)[parnums] <- oldnames
     }
 }
 
