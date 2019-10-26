49c2cd3c243090d49dfceeba29a7631fa5e38378
ecmerkle <merklee@missouri.edu>
2019-10-26 13:57:11 -0500

    fix use of modeest::mlv in ppmc

--- R/ctr_ppmc.R
+++ R/ctr_ppmc.R
@@ -57,7 +57,7 @@ summary.blavPPMC <- function(object, discFUN, dist = c("obs","sim"),
     if ("mode" %in% central.tendency || "map" %in% central.tendency) {
       ## can the modeest package be used?
       if (suppressMessages(requireNamespace("modeest", quietly = TRUE))) {
-        out <- c(out, MAP =  modeest::mlv(x, method = "kernel", na.rm = TRUE)$M)
+        out <- c(out, MAP =  modeest::mlv(x, method = "kernel", na.rm = TRUE)[1])
       } else {
         ## if not, use the quick-and-dirty way
         dd <- density(x, na.rm = TRUE)
