c1b267b0f3d1620098d1b5a31d8254b5b12ec8d1
ecmerkle <merklee@missouri.edu>
2019-11-18 11:28:02 -0600

    Revert "dpriors for targets jags and stanclassic reverted to precisions"
    This reverts commit 495bc991aacc61ca65c05b1b9c971e4432eeed68.

--- R/dpriors.R
+++ R/dpriors.R
@@ -48,9 +48,9 @@ dpriors <- function(..., target="stan"){
 
 jagpriors <- function(nu="dnorm(0,1e-3)", alpha="dnorm(0,1e-2)",
                     lambda="dnorm(0,1e-2)", beta="dnorm(0,1e-2)",
-                    itheta="dgamma(1,.5)", ipsi="dgamma(1,.5)",
+                    itheta="dgamma(1,.5)[sd]", ipsi="dgamma(1,.5)[sd]",
                     rho="dbeta(1,1)", ibpsi="dwish(iden,3)",
-                    tau="dnorm(0,.1)", delta="dgamma(1,.5)"){
+                    tau="dnorm(0,.1)", delta="dgamma(1,.5)[sd]"){
 
   dp <- c(nu=nu, alpha=alpha, lambda=lambda, beta=beta,
           itheta=itheta, ipsi=ipsi, rho=rho, ibpsi=ibpsi,
@@ -77,11 +77,11 @@ stanpriors <- function(nu="normal(0,32)",
 
 stanclassicpriors <- function(nu="normal(0,1000^.5)",
                               alpha="normal(0,10)", lambda="normal(0,10)",
-                              beta="normal(0,10)", itheta="gamma(1,.5)",
-                              ipsi="gamma(1,.5)", rho="beta(1,1)",
+                              beta="normal(0,10)", itheta="gamma(1,.5)[sd]",
+                              ipsi="gamma(1,.5)[sd]", rho="beta(1,1)",
                               ibpsi="wishart(3,iden)",
                               tau="normal(0,10^.5)",
-                              delta="gamma(1,.5)"){
+                              delta="gamma(1,.5)[sd]"){
 
   dp <- c(nu=nu, alpha=alpha, lambda=lambda, beta=beta,
           itheta=itheta, ipsi=ipsi, rho=rho, ibpsi=ibpsi,
