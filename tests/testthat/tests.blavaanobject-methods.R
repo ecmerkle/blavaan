e1dfacc91564fa2f7586654a7fe17c9dbc318b12
ecmerkle <merklee@missouri.edu>
2020-06-09 12:21:04 -0500

    add tests

--- tests/testthat/tests.blavaanobject-methods.R
+++ tests/testthat/tests.blavaanobject-methods.R
@@ -109,9 +109,12 @@ test_that("blavaan object methods work", {
     expect_silent(p <- plot(fitjags, c(2,4), plot.type = "scatter", showplot = FALSE))
   }
 
-  ## ppmc
+  ## blavFit + ppmc
   ppmc_res <- ppmc(fitstan)
   expect_s4_class(ppmc_res, "blavPPMC")
   ppmc_summ <- summary(ppmc_res)
   expect_s3_class(ppmc_summ, "lavaan.data.frame")
+  bf_res <- blavFitIndices(fitstan)
+  expect_s4_class(bf_res, "blavFitIndices")
+  expect_s3_class(summary(bf_res), "lavaan.data.frame")
 })
