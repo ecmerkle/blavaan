daae7ea9779120fb8e7ccdc8c21ddcabe32d0556
ecmerkle <merklee@missouri.edu>
2020-10-22 22:02:58 -0500

    working lv sampling in R

--- R/lvgqs.R
+++ R/lvgqs.R
@@ -12,9 +12,13 @@ lvgqs <- function(modmats, standata) {
   YX <- standata$YX
   usepsi <- standata$usepsi
   nopsi <- standata$nopsi
+  startrow <- standata$startrow
+  endrow <- standata$endrow
+  w9no <- standata$w9no
 
   I <- diag(m)
   precision <- matrix(0, (p+q), (p+q))
+  eta <- with(standata, matrix(NA, Ntot, w9use + w9no))
   
   ## new matrices
   ovmean <- L_Y_A <- vector("list", Ng)
@@ -51,16 +55,14 @@ lvgqs <- function(modmats, standata) {
     ## FIXME?? what if obsidx also extends to x variables?
     obsidx <- Obsvar[mm, ]
     precision[1:Nobs[mm],1:Nobs[mm]] <- solve(top_left[obsidx[1:Nobs[mm]], obsidx[1:Nobs[mm]]])
-    ## FIXME? is chol the same as cholesky_decompose?
-    browser()
     L <- chol(bottom_right[usepsi,usepsi] - (corner[,obsidx[1:Nobs[mm]]] %*% precision[1:Nobs[mm],1:Nobs[mm]] %*% t(corner[,obsidx[1:Nobs[mm]]]))[usepsi,usepsi])
-    beta[, 1:Nobs[mm]] <- corner[, obsidx[1:Nobs[mm]]] %*% precision[1:Nobs[mm], 1:Nobs[mm]]
+    beta <- corner[, obsidx[1:Nobs[mm]]] %*% precision[1:Nobs[mm], 1:Nobs[mm]]
 
     r1 <- startrow[mm]
     r2 <- endrow[mm]
 
     for (idx in r1:r2){
-      lvmean <- modmats[[grpidx]]$alpha + beta[, 1:Nobs[mm]] %*% (YX[idx, 1:Nobs[mm]] - ovmean[grpidx, obsidx[1:Nobs[mm]]])
+      lvmean <- modmats[[grpidx]]$alpha + beta[, 1:Nobs[mm]] %*% (YX[idx, 1:Nobs[mm]] - ovmean[[grpidx]][obsidx[1:Nobs[mm]]])
       eta[idx,usepsi] <- t(rmnorm(1, lvmean[usepsi], sqrt = L));
       if (w9no > 0) {
         eta[idx,nopsi] <- eta[idx,usepsi] %*% t(A[nopsi,usepsi]);
@@ -71,27 +73,38 @@ lvgqs <- function(modmats, standata) {
   eta
 }
 
-matsetup <- function(object, standata, thin = 1) {
-  lavmcmc <- blavInspect(object, 'mcmc')
+samp_lvs <- function(object, standata, thin = 1) {
+  lavmcmc <- make_mcmc(blavInspect(object, 'mcobj'))
   itnums <- sampnums(blavInspect(object, 'mcobj'), thin = thin)
+  nsamps <- length(itnums)
+  nchain <- length(lavmcmc)
 
-  ## TODO change indexing:
-  lavmodel <- fill_params(lavmcmc[[1]][5,], object@Model, object@ParTable)
+  nmat <- object@Model@nmat
+  nblocks <- object@Model@nblocks
 
-  nmat <- lavmodel@nmat
-  nblocks <- lavmodel@nblocks
+  loop.args <- list(X = 1:nsamps, future.seed = TRUE, FUN = function(i){
+      tmpmat <- array(NA, dim=c(nchain, standata$Ntot, standata$w9use + standata$w9no))
+      for(j in 1:nchain){
+        lavmodel <- fill_params(lavmcmc[[j]][i,], object@Model, object@ParTable)
 
-  modmats <- vector("list", nblocks)
-  for (g in 1:nblocks) {
-    ## which mm belong to group g?
-    mm.in.group <- 1:nmat[g] + cumsum(c(0,nmat))[g]
+        modmats <- vector("list", nblocks)
+        for (g in 1:nblocks) {
+          ## which mm belong to group g?
+          mm.in.group <- 1:nmat[g] + cumsum(c(0,nmat))[g]
 
-    modmats[[g]] <- lavmodel@GLIST[ mm.in.group ]
-  }
-browser()
-  eta <- lvgqs(modmats, standata)
+          modmats[[g]] <- lavmodel@GLIST[ mm.in.group ]
+        }
+
+        tmpmat[j,,] <- lvgqs(modmats, standata)
+      }
+      tmpmat})
+
+  etasamps <- do.call("future_lapply", loop.args)
+  etasamps <- array(unlist(etasamps), with(standata, c(nchain, Ntot, w9use + w9no, nsamps)))
+  etasamps <- aperm(etasamps, c(1,4,2,3))
+  etasamps <- lapply(seq(dim(etasamps)[1]), function(x) etasamps[x,,,])
   
-  modmats
+  etasamps
 }
   
 if(FALSE){
@@ -117,5 +130,5 @@ if(FALSE){
               mcmcfile = TRUE)
   load('lavExport/semstan.rda')
 
-  blavaan:::matsetup(fit, stantrans$data)
+  blavaan:::samp_lvs(fit, stantrans$data)
 }
