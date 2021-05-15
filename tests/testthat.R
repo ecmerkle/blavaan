88ba58c941323e5c3b7b3041569d27e681db03f4
ecmerkle <merklee@missouri.edu>
2021-05-15 09:44:57 -0500

    avoid running tests on cran

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library("testthat")
 library("blavaan")
 
-test_check("blavaan")
+## test only if it is a development version
+if (length(strsplit(packageDescription("blavaan")$Version, "\\.")[[1]]) > 2) {
+  test_check("blavaan")
+}
