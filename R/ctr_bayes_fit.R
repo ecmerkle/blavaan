046c93328668a91bf966f2cca3eeb3b86bde8961
ecmerkle <merklee@missouri.edu>
2022-03-01 08:50:10 -0600

    update a fit index test

--- R/ctr_bayes_fit.R
+++ R/ctr_bayes_fit.R
@@ -42,7 +42,7 @@ summary.bfi <- function(object,
 
     if ("mode" %in% central.tendency || "map" %in% central.tendency) {
       ## can the modeest package be used?
-      if (suppressMessages(requireNamespace("modeest", quietly = TRUE)) & length(unique(x)) > 1) {
+      if (suppressMessages(requireNamespace("modeest", quietly = TRUE)) & length(unique(x[!is.na(x)])) > 1) {
         out <- c(out, MAP =  modeest::mlv(x, method = "kernel", na.rm = TRUE))
       } else if(!all(is.na(x))) {
         ## if not, use the quick-and-dirty way
