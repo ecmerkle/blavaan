5760ab54fff8ce7875c02c7e8480efa8628872d0
ecmerkle <merklee@missouri.edu>
2023-10-09 15:36:20 -0500

    refine function for finding blocks in covariance matrices

--- R/blav_utils.R
+++ R/blav_utils.R
@@ -429,13 +429,14 @@ checkcovs <- function(lavobject){
 
 ## check whether model cov matrix is block diagonal
 ## uses matrices from lavInspect(, "free")
-blkdiag <- function(mat) {
+## eqcon is attributes(lavInspect(, "free"))$header
+blkdiag <- function(mat, eqcon = NULL) {
   isblk <- TRUE
   cnum <- 1L
   nblks <- 0
   matrows <- NROW(mat)
-  blkse <- matrix(0, matrows, 3) # start/end of each block, and first free param in that block
-  while (isblk && cnum <= matrows) {
+  blkse <- matrix(0, matrows, 3) # start/end of each block, is it a fully unrestricted block
+  while (cnum <= matrows) {
     ## ending row of this potential block
     currend <- which(mat[, cnum] > 0)
     if (length(currend) > 0) {
@@ -445,6 +446,7 @@ blkdiag <- function(mat) {
       currend <- cnum
     }
 
+    nblks <- nblks + 1
     if (currend > cnum) {
       ## check that columns cnum+1 to max() also equal max()
       othend <- sapply((cnum + 1):currend, function(i) {
@@ -455,31 +457,31 @@ blkdiag <- function(mat) {
           out <- i
         }
         out})
-        
+
       if (all(othend == currend)) {
         ## is this entire submatrix free?
         submat <- mat[cnum:currend, cnum:currend]
-        if (all(submat[lower.tri(submat)] > 0)) {
-          nblks <- nblks + 1
-          blkse[nblks,] <- c(cnum, currend, min(submat[lower.tri(submat)]))
-          cnum <- currend + 1
+        submat <- submat[lower.tri(submat)]
+        if (all(submat > 0) & all(!(submat %in% as.numeric(eqcon$rhs)))) {
+          blkse[nblks,] <- c(cnum, currend, TRUE)
         } else {
           isblk <- FALSE
+          blkse[nblks,] <- c(cnum, currend, FALSE)
         }
       } else {
         isblk <- FALSE
+        blkse[nblks,] <- c(cnum, currend, FALSE)
       }
     } else if (currend == cnum) {
       ## 1x1 block
-      nblks <- nblks + 1
-      blkse[nblks,] <- c(cnum, cnum, 0)
-      cnum <- cnum + 1
+      blkse[nblks,] <- c(cnum, cnum, TRUE)
     } else {
       isblk <- FALSE
     }
+    cnum <- currend + 1
   }
 
-  if (isblk) blkse <- blkse[1:nblks, , drop = FALSE]
+  blkse <- blkse[1:nblks, , drop = FALSE]
   
   list(isblk = isblk, nblks = nblks, blkse = blkse)
 }
